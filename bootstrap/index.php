d725be7e5e8a8463bb44a2b528199a406dd83617
ShOpp3RS <ponxiito_007@hotmail.com>
2017-12-04 03:42:04 -0300

    Add files via upload

---
+++ bootstrap/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
+
+	<title>penepne</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+<header>
+	<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toogle="collapse" data-target="#navegacion-fm">
+					<span class="sr-only">Desplegar / Ocultar Menu</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+			
+
+				</button>
+				<a href="#" class="navbar-brand">EstacionMeteorologica</a>
+			</div>
+			<!--inicia Menu-->
+			<div class="collapse navbar-collapse" id="navegacion-fm">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Inicio</a></li>
+					<li class="dropdown"><a href="#" class="dropdown-toggle" data-toogle="dropdown" role="button">Categorias <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="Menu">
+							<li ><a href="#">blog</a></li>
+							<li class="divider"></li>
+							<li ><a href="#">afgd</a></li><li class="divider"></li>
+							<li ><a href="#">sssg</a></li><li class="divider"></li><li ><a href="#">dddd</a></li><li class="divider"></li>
+							
+						</ul>
+					</li>
+					<li ><a href="#">blog</a></li>
+					<li ><a href="#">contacto</a></li>
+				</ul>
+<form action="" class="navbar-form navbar-right" role="search"><div class="form-group">
+	<input type="text" class="form-control" placeholder="buscar">
+</div>
+<button type="submit" class="btn btn-primary">
+	<span class="glyphicon glyphicon-search"></span>
+</button>
+</form>
+
+			</div>
+		</div>
+		
+	</nav>
+</header>
+<section class="jumbotron">
+	<div class="container">
+		<h1 class="titulo-blog">Estacion Meteorologica</h1>
+		<p>pene</p>
+		
+	</div>
+</section>
+<section class="main container"></section>
+<footer></footer>
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js" ></script>
+</body>
+</html>
