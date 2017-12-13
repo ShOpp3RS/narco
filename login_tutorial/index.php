e4cfd4ba007d66e1cae2993422e9a92d12296f95
ShOpp3RS <ponxiito_007@hotmail.com>
2017-12-13 01:41:24 -0300

    Add files via upload

---
+++ login_tutorial/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>login con php</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+<div class="error">
+	<span>datos de ingreso no validos. intentelo de nuevo por favor</span>
+</div>
+<div class="main">
+	<form action="" id="formlg">
+		<input type="text" name="usuariolg" placeholder="Usuario" required />
+		<input type="password" name=" passlg" placeholder="Constraseña" required/>
+		<input type="submit" name="botonlg" value="iniciar sesion"/>
+	</form>
+</div>
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
